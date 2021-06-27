27126b0da5da8c4868ff1b04e3adc5cfdf2e0334
Fawekes73 <fawekeszephyr73@gmail.com>
2021-06-28 01:09:25 +0530

    solved error of added loading and error message to the show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,16 +7,50 @@ const Show = () => {
 
   const [show, setShow] = useState(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [error, SetError] = useState(null);
+
   useEffect(() => {
+
+    let isMounted = true;
 
     apiGet(`/shows/1?embed${id}=seasons&embed[]=cast`).then(results => {
 
-      setShow(results);
-    })
 
+      if (isMounted) {
+
+        setShow(results);
+        setIsLoading(false);
+      }
+
+
+
+
+    }).catch(err => {
+      if (isMounted) {
+        SetError(err.message);
+        setIsLoading(false);
+      }
+
+    });
+
+    return () => {
+      isMounted = false;
+    }
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>
+      Data is Being Loaded
+    </div>
+  }
+  if (error) {
+    return <div>Error Occured: {error}</div>
+  }
+
   return (
     <div>
       This is show page
