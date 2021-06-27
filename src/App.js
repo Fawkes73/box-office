1e07bfe9be0e768632de3dc51297434af9d14aed
Fawekes73 <fawekeszephyr73@gmail.com>
2021-06-27 23:48:18 +0530

    correcter the added show page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import Show from './pages/Show'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 function App() {
@@ -13,6 +13,8 @@ function App() {
         <Starred />
       </Route>
       <Route exact path="/show/:id">
+
+        <Show />
 
       </Route>
       <Route>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,63 +1,27 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 import { apiGet } from '../misc/config';
-
 const Show = () => {
 
   const { id } = useParams();
+
   const [show, setShow] = useState(null);
-  const [isLoading, setIsLoading] = usState(true);
-  const [error, setError] = useState(null);
-
-
 
   useEffect(() => {
 
-    let isMounted = true;
+    apiGet(`/shows/1?embed${id}=seasons&embed[]=cast`).then(results => {
 
-    apiGet(`/shows/$[id]?embed[]=seasons&embed[]=cast`).then(results => {
-
-
-      () => {
-        if (isMounted) {
-          setShow(results);
-
-          setIsLoading(false);
-        }
-
-      }
-
-    }).catch(err => {
-      if (isMounted) {
-        setError(err.message);
-        setIsLoading(false);
-      }
-
-    });
-
-
-
-    return () => {
-      isMounted = false;
-    }
+      setShow(results);
+    })
 
   }, [id]);
 
-
-  if (isLoading) {
-    return <div>Data is Being Loaded</div>
-  }
-
-  if (error) {
-    return <div>Error Occured: {error}</div>
-  }
-
+  console.log('show', show);
   return (
     <div>
-      This is Show Page
-
+      This is show page
     </div>
   )
-};
+}
 
 export default Show
