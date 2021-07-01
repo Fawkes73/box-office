b34a65fddd5e2b45eba45b3e1588aee1c997f950
Fawkes73 <fawkeszephyr73@gmail.com>
2021-07-01 22:10:12 +0530

    Updates

---
+++ precache-manifest.6417800030c152ed95fed49d1eb868c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b248e4b91fab436cb405c8c13cd040c",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "7457877522ad02c99849",
+    "url": "/box-office/static/css/main.3eb0ca28.chunk.css"
+  },
+  {
+    "revision": "308af7d11fcde485fca0",
+    "url": "/box-office/static/js/2.b952be28.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.b952be28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7457877522ad02c99849",
+    "url": "/box-office/static/js/main.2b926b1b.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
